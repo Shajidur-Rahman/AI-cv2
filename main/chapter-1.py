dfe04dad913e175839ccc3e29a5a61482448b807
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-08-21 22:41:06 +0600

    video continue 17

--- main/chapter-1.py
+++ main/chapter-1.py
@@ -1,3 +1,14 @@
 import cv2
 
 print("packages imported")
+
+# reading images
+"""
+img = cv2.imread("../resources/Photos/cat.jpg")
+cv2.imshow("Cat", img)
+
+cv2.waitKey(0)
+"""
+
+
+capture = cv2.VideoCapture(0)
