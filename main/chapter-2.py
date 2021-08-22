da53f7c8e0aba84749bdf85afb929011bf109bba
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-08-22 17:16:34 +0600

    chapter-2.py finished and video continue 27:23

--- main/chapter-2.py
+++ main/chapter-2.py
@@ -1,9 +1,19 @@
 import cv2
+import numpy as np
 
 img = cv2.imread("../resources/Photos/cat.jpg")
+kernal = np.ones((5, 5), dtype=np.uint8)
 
 gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+img_blur = cv2.GaussianBlur(gray_img, (7, 7), 0)
+img_canny = cv2.Canny(img, 100, 100)
+img_dialation = cv2.dilate(img_canny, kernal, iterations=1)
+img_eroded = cv2.erode(img_dialation, kernal, iterations=1)
 
-cv2.imshow("rgb", gray_img)
+# cv2.imshow("gray", gray_img)
+# cv2.imshow("blur", img_blur)
+# cv2.imshow("canny", img_canny)
+cv2.imshow("dialatiuon", img_dialation)
+cv2.imshow("eroded", img_eroded)
 
 cv2.waitKey(0)
---
+++ main/try.py
@@ -0,0 +1,21 @@
+import cv2
+
+capture = cv2.VideoCapture(0)
+capture.set(10,100)
+
+
+# while True:
+#     _, frame = capture.read()
+#     cv2.imshow("video", frame)
+#
+#     if cv2.waitKey(0) == ord("q"):
+#         break
+
+
+while True:
+    _, frame = capture.read()
+    frame = cv2.Canny(frame, 100, 100)
+    cv2.imshow("Video", frame)
+
+    if cv2.waitKey(1) & 0xff == ord("q"):
+        break
